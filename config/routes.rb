207e3cf28c5377fe5bf0c3e568d162b60d6efc84
Anisha <anisha1999.gupta@gmail.com>
2023-10-26 00:59:01 -0400

    tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rottenpotatoes::Application.routes.draw do
   resources :movies
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/movies')
-  get ':controller/:action/:id'
-  post ':controller/:action/:id'
+  # get ':controller/:action/:id'
+  # post ':controller/:action/:id'
+  get '/movies/samedirector/:id', to: 'movies#samedirector', as: :samedirector
 end
