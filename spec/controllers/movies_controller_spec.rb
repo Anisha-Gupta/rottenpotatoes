9b98a96c095c8bef3cdf2beeea5afd6767207f3c
Anisha <anisha1999.gupta@gmail.com>
2023-10-25 23:22:31 -0400

    fixing tests

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -1,17 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe MoviesController, type: :controller do
-describe "GET 'same_director'" do
-    it "should loads movies of same director into @movies" do
-      get :same_director, :id => @StarWars.id
+describe 'samedirector' do
+    it "should load movies of same director into @movies" do
+      get :samedirector, params: { id: @StarWars.id }
       assigns(:movies).should include(@THX1138)
     end
     it "should not include movies of different director into @movies" do
-      get :same_director, :id => @StarWars.id
+      get :samedirector, params: { id: @StarWars.id }
       assigns(:movies).should_not include(@BladeRunner)
     end
     it 'flashes a message for a movie with no director info' do
-      get :same_director, :id => @Alien.id
+      movie = Movie.create(title: 'Movie Title', director: '')
+      get :samedirector, params: { id: movie.id }
       expect(flash[:notice]).to include("'#{movie.title}' has no director info.")
       expect(response).to redirect_to(movies_path)
     end
--- spec/models/models_spec.rb
+++ spec/models/models_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe MoviesController, type: :controller do
-describe Movie do
+RSpec.describe Movie, type: :model do
   it "function should return movies of same director" do
     @StarWars.find_all_by_director.should include(@THX1138)
   end
@@ -9,5 +8,4 @@ describe Movie do
   it "function should return movies of same director" do
     @StarWars.find_all_by_director.should_not include(@BladeRunner)
   end
-end
 end
