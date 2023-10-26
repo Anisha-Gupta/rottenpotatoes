1b25c0108901c3532beb64c96e03ac14b8d5bb64
Anisha <anisha1999.gupta@gmail.com>
2023-10-26 00:50:00 -0400

    tests

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 RSpec.describe MoviesController, type: :controller do
   describe 'samedirector' do
     it 'redirects to movies_path when the movie has no director info' do
-      movie = Movie.create(title: 'Movie Title', director: '') # Create a movie with no director info
+      movie = Movie.create(title: 'Alien', director: '') # Create a movie with no director info
       get :samedirector, params: { id: movie.id }
       expect(flash[:notice]).to eq("'#{movie.title}' has no director info.")
       expect(response).to redirect_to(movies_path)
     end
 
     it 'assigns movies with the same director' do
-      director = 'Director Name'
-      movie1 = Movie.create(title: 'Movie 1', director: director)
-      movie2 = Movie.create(title: 'Movie 2', director: director)
+      director = 'George Lucas'
+      movie1 = Movie.create(title: 'Star Wars', director: director)
+      movie2 = Movie.create(title: 'THX-1138', director: director)
 
       get :samedirector, params: { id: movie1.id }
 
@@ -20,7 +20,7 @@ RSpec.describe MoviesController, type: :controller do
     end
 
     it 'renders the samedirector template' do
-      movie = Movie.create(title: 'Movie Title', director: 'Director Name')
+      movie = Movie.create(title: 'Blade Runner', director: 'Ridley Scott')
       get :samedirector, params: { id: movie.id }
       expect(response).to render_template('samedirector')
     end
